29ba77321acf21604737a5274daeb026808838bb
Jimmy <vermeulen.jimmy@gmail.com>
2020-01-03 17:23:56 +0100

    Fix movie item layout and number Fix year input

--- app/src/main/java/com/example/movielist/MovieAdapter.kt
+++ app/src/main/java/com/example/movielist/MovieAdapter.kt
@@ -32,7 +32,7 @@ class MovieAdapter(private val movies: List<Results>, private val onClick: (Resu
         }
 
         fun bind(movieItem: Results) {
-            itemView.tvPosition.text = movieItem.title
+            itemView.tvPosition.text = context.getString(R.string.movie_position, adapterPosition+1)
             Glide.with(context).load(movieItem.getImageUrl()).into(itemView.ivPoster)
         }
     }
